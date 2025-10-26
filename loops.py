88851f07240351cb829431eade832e5a1637093b
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-10-26 09:35:16 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,14 @@
+
+for letter in "jashwanth kumar reddy":
+    if letter == 'y':
+        continue
+    print(letter, end=' ')
+print()
+
+
+for letter in "Python":
+    if letter == 'y':
+        break
+    print(letter, end=' ')
+print()
+
