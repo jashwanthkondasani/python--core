39eb15a59acfbb301dd05e9c2f7349530b2f626c
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-10-26 09:37:08 +0530

    Add loop examples and prime checking functionality
    Added examples of continue, break, multiplication table, and prime checking.

--- loops.py
+++ loops.py
@@ -1,14 +1,25 @@
 
+# continue the word
 for letter in "jashwanth kumar reddy":
     if letter == 'y':
         continue
     print(letter, end=' ')
 print()
-
+# break the word
 
 for letter in "Python":
     if letter == 'y':
         break
     print(letter, end=' ')
 print()
+# print multiplication table
+n=int(input("enter the number"))
+for i in range(1,11):
+      print(f"{n}X{i}={n*i}")
 
+# check weather prime or not
+for i in range (2,n):
+      if(n%i)==0:
+            print(" number is not prime")
+      else:
+            print("number is not prime")
