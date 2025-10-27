cf84508629977a95d81662ba89fbbfd8883e1320
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-10-27 21:41:18 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -53,3 +53,15 @@ def power(n,p):
                 return 1
         return n*power(n,p-1)
 print(power(11,22))
+
+#  check wether number is prime or not prime\
+
+def prime(n,i=2):
+      if n<=2:
+            return True if n==2 else False
+      if n%i==0:
+            return False
+      if i*i>n:
+            return True
+      return prime(n,i+1)   
+print(prime(22))
