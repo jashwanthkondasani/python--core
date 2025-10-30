99d45d0594f324de6a80dcd2c3d3230b8b5be0b2
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-10-30 21:11:31 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -89,3 +89,18 @@ elif num == 0:
     print("Number is Zero")
 else:
     print("Number is Negative")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b:
+    if a > c:
+        print("a is the largest")
+    else:
+        print("c is the largest")
+else:
+    if b > c:
+        print("b is the largest")
+    else:
+        print("c is the largest")
