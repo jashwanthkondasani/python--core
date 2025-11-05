ecb1f4a5cda1edf770a1dc090499211ba527936f
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-11-05 21:04:42 +0530

    Create types.py

---
+++ types.py
@@ -0,0 +1,34 @@
+#  srting by select strings \
+# s="nani kondasani"
+# print(s[0]) 
+
+# s = "GeeksforGeeks"
+# print(s[1:4])    # characters from index 1 to 3
+# print(s[:3])     # from start to index 2
+# print(s[3:])     # from index 3 to end
+# print(s[::-1])   # reverse string
+
+
+# s="allu arjumn" 
+# for char in s:    # characters from index 0 to
+#     print(char)
+
+# s = "nani kondasani"
+# s="n"+s[1:]
+# print(s)
+
+# s = "ruturaj gaikwad"
+# s1 = "r" + s[1:]                   # update first character
+# s2 = s.replace("geeks", "GeeksforGeeks")  # replace word
+# print(s1)\
+# print(s2)
+
+# convert into upper and lower cases \
+# s="CHANGE"
+# print((s.lower()))
+
+# s="change matters "
+# print((s.upper ()))
+
+# s = "My name is {} and I am {} years old.".format("jashwanth kondasani ", 22)
+# print(s)
