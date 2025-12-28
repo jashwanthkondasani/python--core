bf233bad3bc49782c7e0c9bfd3141bbbd33c883e
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-12-28 09:48:58 +0530

    Create exception.py

---
+++ exception.py
@@ -0,0 +1,40 @@
+# try:
+#    num=int(input("enter a number "))
+#    print(100/num)
+# except ZeroDivisionError:
+#    print("Error: Division by zero is not allowed.")
+# else:
+#    print("execution successful")
+
+
+# while True:
+#       try:
+#           age=int(input("enter your age "))
+#           if age<=0:
+#               print("Error: Invalid age. Age must be positive.")
+#           else:
+#               print("Age is valid.")
+#       except ValueError:
+#           print("Error: Invalid input. Please enter a numeric value.")
+
+
+# while True:
+#       try:
+#            age=int(input("enter your age "))
+#            if age>=18:
+#                 print("you are eligible for vote ")
+#            else:
+#                 print("you are not eligible for vote ")
+#       except ValueError:
+#            print("Error: Invalid input. Please enter a numeric value.")
+
+
+# def divide(a,b):
+#    return a/b
+# def calculate():
+#    return divide(10,80)
+# try:
+#    calculate()
+#    print("Calculation successful")
+# except ZeroDivisionError:
+#    print("Error: Division by zero is not allowed.")
