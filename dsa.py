faedea2b6145cd4392d6528a1b0e734cdeb8073f
jashwanthkondasani <jashwanthkumarreddy53@gmail.com>
2025-12-29 22:17:07 +0530

    Update dsa.py

--- dsa.py
+++ dsa.py
@@ -17,3 +17,31 @@ print("Top element in the stack:", topElement)
 
 poppedElement = stack.pop() if stack else None
 print("Popped element from the stack:", poppedElement)
+
+queue=[]
+queue.append(10)
+queue.append(20)
+queue.append(30)
+print("Queue after enqueuing elements:", queue)
+
+frontElement = queue[0] if queue else None
+print("Front element in the queue:", frontElement)
+
+dequeuedElement = queue.pop(0) if queue else None
+print("Dequeued element from the queue:", dequeuedElement)
+
+class car:
+   def __init__(self,color,price):
+       self.color=color
+       self.price=price
+
+car1=car("red", 1000000)
+car2=car("blue", 2000000)
+car3=car("green", 3000000)
+
+print("car color:", car1.color)
+print("car color:", car2.color)
+print("car color:", car3.color)
+print("car price:", car1.price)
+print("car price:", car2.price)
+print("car price:", car3.price)
